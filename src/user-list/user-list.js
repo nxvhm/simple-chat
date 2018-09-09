c504688079ca537fa38b6b63c2aab3ede721dd26
unknown <a.partadzhiev@gmail.com>
2018-09-09 23:13:32 +0300

    playing with user list

---
+++ src/user-list/user-list.js
@@ -0,0 +1,74 @@
+import React, {Component} from 'react'
+import { List, Image } from 'semantic-ui-react'
+
+class UserList extends Component {
+  render() {
+    return(
+      <div className='user-list-container bg-black'>
+        <List>
+          <List.Item className="chat-user-item">
+            <Image avatar src='/images/avatar/small/rachel.png' />
+            <List.Content>
+              <List.Header as='a'>Rachel</List.Header>
+              <List.Description>
+                Last seen watching{' '}
+                <a>
+                  <b>Arrested Development</b>
+                </a>{' '}
+                just now.
+              </List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item className="chat-user-item">
+            <Image avatar src='/images/avatar/small/lindsay.png' />
+            <List.Content>
+              <List.Header as='a'>Lindsay</List.Header>
+              <List.Description>
+                Last seen watching{' '}
+                <a>
+                  <b>Bob's Burgers</b>
+                </a>{' '}
+                10 hours ago.
+              </List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item className="chat-user-item">
+            <Image avatar src='/images/avatar/small/matthew.png' />
+            <List.Content>
+              <List.Header as='a'>Matthew</List.Header>
+              <List.Description>
+                Last seen watching{' '}
+                <a>
+                  <b>The Godfather Part 2</b>
+                </a>{' '}
+                yesterday.
+              </List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item className="chat-user-item">
+            <Image avatar src='/images/avatar/small/jenny.jpg' />
+            <List.Content>
+              <List.Header as='a'>Jenny Hess</List.Header>
+              <List.Description>
+                Last seen watching{' '}
+                <a>
+                  <b>Twin Peaks</b>
+                </a>{' '}
+                3 days ago.
+              </List.Description>
+            </List.Content>
+          </List.Item>
+          <List.Item className="chat-user-item">
+            <Image avatar src='/images/avatar/small/veronika.jpg' />
+            <List.Content>
+              <List.Header as='a'>Veronika Ossi</List.Header>
+              <List.Description>Has not watched anything recently</List.Description>
+            </List.Content>
+          </List.Item>
+          </List>
+      </div>
+    )
+  }
+}
+
+export default UserList
