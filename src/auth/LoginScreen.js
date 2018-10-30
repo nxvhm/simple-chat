17a8e702a5e2fa9907114e8a858762d80e47121e
barbequin <a.partadzhiev@gmail.com>
2018-10-30 23:14:36 +0200

    add succes/warning messages @ LoginScreen.js

--- src/auth/LoginScreen.js
+++ src/auth/LoginScreen.js
@@ -13,15 +13,31 @@ class LoginScreen extends Component {
 
           emailValid: false,
           passwordValid: false,
-
           requestError: false,
-
+          warningMsg: false,
+          warningMsgContent: '',
+          successMsg: false,
+          successMsgContent: '',
           formErrors: {email: '', password: ''}
       }
 
       this.handleSubmit     = this.handleSubmit.bind(this);
-      this.handleUserInput  = this.handleUserInput.bind(this);      
+      this.handleUserInput  = this.handleUserInput.bind(this);  
+      this.handleDismiss  = this.handleDismiss.bind(this);      
+
     }
+    componentWillMount() {
+      // Location Props
+      let locProps = this.props.location;
+
+      let successMsg = locProps.state && locProps.state.successMsg
+        ? true : false; 
+
+      let successMsgContent = locProps.state && locProps.state.successMsgContent
+      ? locProps.state.successMsgContent : ''; 
+                  
+      this.setState({successMsg, successMsgContent});
+    }    
     // Update state when user inputs data
     handleUserInput(e) {
       const name = e.target.name;
@@ -68,21 +84,41 @@ class LoginScreen extends Component {
       }
 
       let apiUrl = process.env.REACT_APP_API_URI; 
-
+      let self = this;
       axios.post(`${apiUrl}/login`, {
         email: this.state.email,
         password: this.state.password
       }).then(function(response){
         console.log(response);
+        if (response.data.error) {
+          self.setState({warningMsg: true});
+        }
       });
 
     }
 
+    handleDismiss(e) {
+      let target = e.target.parentNode.attributes.name.value;
+      let state = this.state;
+      state[target] = !state[target];
+      this.setState({state})
+    }
+
     render() {
+      // this.props.location.state.successMsgContent;      
       return(
         <Container>
           <h1 className="text-center">Login</h1>
-            <Message
+
+            <Message name="successMsg" hidden={!this.state.successMsg}
+              onDismiss={this.handleDismiss}
+              success
+              header='Success !'
+              content={this.state.successMsgContent}
+            />
+
+            <Message name="warningMsg" hidden={!this.state.warningMsg}
+              onDismiss={this.handleDismiss}
               warning
               header='You must register before you can do that!'
               content='Visit our registration page, then try again.'
